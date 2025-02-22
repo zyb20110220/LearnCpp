b9831ee7dbf05ed93c9dcab00de8d8740b9eec8d
张彦博 <2120056024@qq.com>
2025-02-22 12:27:08 +0000

    2.22(5)

---
+++ 2025.2/2.22/OJ3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	long long a, b;
+	cin >> a >> b;
+	if ((a + b) <= 666)
+	{
+		cout << "就这";
+	}
+	else
+	{
+		cout << "多了多了";
+	}
+	return 0;
+}
---
+++ 2025.2/2.22/OJ4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	cin >> a;
+	if (a % 3 == 0)
+	{
+		cout << "yes";
+	}
+	else
+	{
+		cout << "no";
+	}
+	return 0;
+}
